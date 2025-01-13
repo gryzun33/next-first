5cb1363cbd0104bc328413839bc42e004d0f76e1
gryzun33 <yakushevaolga564@gmail.com>
2025-01-13 12:31:48 +0300

    feat: add loading and error

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -6,14 +6,33 @@ type Props = {
   };
 };
 
-export async function generateMetadata({
-  params: { id },
-}: Props): Promise<Metadata> {
+async function getData(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  const data = await response.json();
+  return data;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
   return {
     title: `Post ${id}`,
   };
 }
 
-export default function Post({ params: { id } }: Props) {
-  return <h1>Post page {id}</h1>;
+export default async function Post({ params }: Props) {
+  const { id } = await params;
+  const post = await getData(id);
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
+  );
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,31 @@
-export default function Blog() {
-  return <h1>Blog page</h1>;
+import Link from "next/link";
+
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: {
+      revalidate: 60,
+    },
+  });
+
+  // if (!response.ok) {
+  //   throw new Error("OH MY GOD!!!!");
+  // }
+  const data = await response.json();
+  return data;
+}
+
+export default async function Blog() {
+  const posts = await getData();
+  return (
+    <div>
+      <h1>Blog page</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
